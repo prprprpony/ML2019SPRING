f8916904c3de4cda7d1397035100fd0cf5293159
prpr <3254tg89@gmail.com>
2019-02-24 16:35:38 +0800

    add hw1.sh

--- hw1/predict.py
+++ hw1/predict.py
@@ -1,20 +1,23 @@
+import sys
 import csv
 import numpy as np
 # preprocess data
-raw_data = list(csv.reader(open('test.csv'),delimiter=','))
+fin = open(sys.argv[1],'r')
+fout = open(sys.argv[2],'w')
+raw_data = list(csv.reader(fin,delimiter=','))
 K = 9 * 18 + 1 # 9 hours * 18 fatures + bias
 w = np.load('weight.npy')
-f = csv.writer(open('ans.csv','w'), delimiter=',')
+f = csv.writer(fout, delimiter=',')
 f.writerow(['id','value'])
-print(w)
 for i in range(0,len(raw_data),18):
     x = []
     for j in range(18):
         x += raw_data[i+j][2:]
+    s = raw_data[i][0]
     x += [1]
     for j in range(len(x)):
         if x[j] == 'NR':
             x[j] = '0'
     x = np.array(x,dtype='float64')
     y = np.dot(w,x)
-    f.writerow(['id_'+str(i//18), y])
+    f.writerow([s, y])
