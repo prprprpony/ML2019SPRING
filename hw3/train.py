35464cb6a353ea1c262e552505d6ee3077bd768c
prpr <3254tg89@gmail.com>
2019-03-27 18:22:08 +0800

    [hw3] CNN: change model

--- hw3/train.py
+++ hw3/train.py
@@ -6,41 +6,43 @@ from keras.models import Sequential
 from keras.layers import Conv2D,MaxPooling2D,Flatten,Dense,Dropout,BatchNormalization
 # initlize model
 model = Sequential()
-model.add(Conv2D(32, (3,3), input_shape = (48,48,1), activation = 'relu'))
+model.add(Conv2D(32, (3,3), input_shape = (48,48,1), activation = 'elu'))
+model.add(BatchNormalization())
+model.add(Conv2D(32, (3,3), activation = 'elu'))
 model.add(BatchNormalization())
 model.add(MaxPooling2D(pool_size = (2, 2)))
 model.add(Dropout(0.2))
 
-model.add(Conv2D(32, (3,3), activation = 'relu'))
+model.add(Conv2D(64, (3,3), activation = 'elu'))
+model.add(BatchNormalization())
+model.add(Conv2D(64, (3,3), activation = 'elu'))
 model.add(BatchNormalization())
 model.add(MaxPooling2D(pool_size = (2, 2)))
 model.add(Dropout(0.3))
 
-model.add(Conv2D(32, (3,3), activation = 'relu'))
+model.add(Conv2D(128, (3,3), activation = 'elu'))
+model.add(BatchNormalization())
+model.add(Conv2D(128, (3,3), activation = 'elu'))
 model.add(BatchNormalization())
 model.add(MaxPooling2D(pool_size = (2, 2)))
 model.add(Dropout(0.4))
 
 model.add(Flatten())
-model.add(Dense(units = 512, activation ='relu'))
-model.add(BatchNormalization())
-model.add(Dropout(0.5))
-model.add(Dense(units = 512, activation ='relu'))
-model.add(BatchNormalization())
-model.add(Dropout(0.5))
-model.add(Dense(units = 512, activation ='relu'))
-model.add(BatchNormalization())
-model.add(Dropout(0.5))
 model.add(Dense(units = 7, activation ='softmax'))
+
 model.compile(optimizer = 'adam', loss = 'categorical_crossentropy', metrics = ['accuracy'])
 print(model.summary())
+#exit(0)
 # preprocess data
 x_data = pd.read_csv(os.popen("tail -n +2 " + sys.argv[1] + " | sed 's/,/ /g'"), sep=' ', header=None).values
 y_data = to_categorical(x_data[:,0], 7)
 x_data = np.delete(x_data,0,1) / 255
 x_data = x_data.reshape((x_data.shape[0],48,48,1))
 # training
-model.fit(x_data, y_data,batch_size=100,epochs=120)
+try:
+    model.fit(x_data, y_data,batch_size=100,epochs=60)
+except:
+    model.save(str(int(time.time())) + '-CNN-crashed.h5')
 result = model.evaluate(x_data,y_data)
 print(result)
 accuracy = result[1]
