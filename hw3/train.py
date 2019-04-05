86ed68073cab5c7164cb137e6d89bc2812c6452c
prpr <3254tg89@gmail.com>
2019-04-05 13:35:09 +0800

    [hw3] add ImageDataGenerator

--- hw3/train.py
+++ hw3/train.py
@@ -5,6 +5,7 @@ from keras.utils import to_categorical
 from keras.models import Sequential
 from keras.layers import Conv2D,MaxPooling2D,Flatten,Dense,Dropout,BatchNormalization
 from keras.models import load_model
+from keras.preprocessing.image import ImageDataGenerator
 # initlize model
 model = None
 if len(sys.argv) == 3:
@@ -34,6 +35,10 @@ else:
     model.add(Dropout(0.4))
 
     model.add(Flatten())
+    model.add(Dense(units = 512, activation ='elu'))
+    model.add(BatchNormalization())
+    model.add(Dense(units = 512, activation ='elu'))
+    model.add(BatchNormalization())
     model.add(Dense(units = 7, activation ='softmax'))
 
     model.compile(optimizer = 'adam', loss = 'categorical_crossentropy', metrics = ['accuracy'])
@@ -44,9 +49,18 @@ x_data = pd.read_csv(os.popen("tail -n +2 " + sys.argv[1] + " | sed 's/,/ /g'"),
 y_data = to_categorical(x_data[:,0], 7)
 x_data = np.delete(x_data,0,1) / 255
 x_data = x_data.reshape((x_data.shape[0],48,48,1))
+datagen = ImageDataGenerator(
+        rotation_range=40,
+        width_shift_range=0.2,
+        height_shift_range=0.2,
+        shear_range=0.2,
+        zoom_range=0.2,
+        horizontal_flip=True,
+        fill_mode='nearest')
 # training
 try:
-    model.fit(x_data, y_data,batch_size=100,epochs=60)
+    #model.fit(x_data, y_data,batch_size=100,epochs=150)
+    model.fit_generator(datagen.flow(x_data, y_data,batch_size=100),steps_per_epoch=len(x_data)*2//100,epochs=150,workers=10)
 except:
     model.save(datetime.datetime.now().strftime("%m-%d-%H:%M:%S") + '-CNN-crashed.h5')
 result = model.evaluate(x_data,y_data)
