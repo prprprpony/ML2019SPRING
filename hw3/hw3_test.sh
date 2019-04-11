80d32c317b05305db9043f31e0c8c8a80686a827
prpr <3254tg89@gmail.com>
2019-04-11 23:55:06 +0800

    [hw3] fix hw3_test.sh

--- hw3/hw3_test.sh
+++ hw3/hw3_test.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget 'https://www.dropbox.com/s/227ofca098zjjzu/final-model.h5?dl=1'
+wget 'https://www.dropbox.com/s/227ofca098zjjzu/final-model.h5?dl=1' -O final-model.h5
 python predict.py $1 $2 final-model.h5
